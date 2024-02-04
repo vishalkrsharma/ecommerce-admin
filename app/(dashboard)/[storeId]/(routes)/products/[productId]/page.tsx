a439a703492eef0d1afe21d4a568ad69165bc6cc
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-02-04 19:16:10 +0530

    fix: key errors

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -104,7 +104,10 @@ const ProductInfoPage = async ({ params }: { params: { productId: string } }) =>
         />
         <div className='my-8 lg:grid lg:grid-cols-3 max-lg:space-y-4 gap-4'>
           {product.reviews.map((review: Review) => (
-            <div className='border px-4 py-3 rounded-lg items-center'>
+            <div
+              key={review.id}
+              className='border px-4 py-3 rounded-lg items-center'
+            >
               <div className='flex justify-start items-center space-x-2'>
                 {review.sentiment === 'GOOD' ? <ThumbsUp className='text-gray-300' /> : <ThumbsDown className='text-gray-300' />}
                 <p>{formatDate(review.createdAt)}</p>
